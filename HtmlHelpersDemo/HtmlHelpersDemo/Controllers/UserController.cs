56ea5c0b056c2a7327bbab2ab918d0d64d9a964c
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2022-01-19 18:30:52 +0530

    pushing new project

---
+++ HtmlHelpersDemo/HtmlHelpersDemo/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HtmlHelpersDemo.Models;
+namespace HtmlHelpersDemo.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult Register()
+        {
+            LoadContries();
+            return View();
+        }
+
+        [NonAction]
+        public void LoadContries()
+        {
+            var countries = new List<SelectListItem>
+                    {
+                new SelectListItem{Value="India",Text="India" },
+                new SelectListItem{Value="U.S.",Text="U.S." },
+                new SelectListItem{Value="China",Text="China" }
+                };
+            ViewBag.country = countries;
+        }
+
+        [HttpPost]
+        public ActionResult Save(Register register)
+        {
+            if (register.Terms)
+            {
+                // Your save logic goes here
+                return Content("Registered Successfully");
+            }
+            else
+            {
+                ViewBag.msg = "You have to agreed upon Terms";
+                LoadContries();
+                return View("Register");
+            }
+        }
+    }
+}
---
+++ HtmlHelpersDemo/HtmlHelpersDemo/Models/Register.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HtmlHelpersDemo.Models
+{
+    public class Register
+    {
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+
+        public string Password { get; set; }
+
+        public string Country { get; set; }
+
+        public bool Terms { get; set; }
+    }
+}
